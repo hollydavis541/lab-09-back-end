079e36e9b107b6d0cda76bb06b1c6b3e6bbbd054
Holly Davis <hollydavis541@gmail.com>
2019-11-09 09:11:26 -0800

    added comments and logs for API testing

--- server.js
+++ server.js
@@ -1,21 +1,22 @@
 'use strict';
 
+//Load environment variables from the .env file
 require('dotenv').config();
 
-//Dependencies and setup
-const express = require('express');
-const cors = require('cors');
-const superagent = require('superagent');
-const pg = require('pg');
-const PORT = process.env.PORT || 3000;
-const app = express();
-app.use(cors());
+//Dependencies and Setup
+const express = require('express'); //Express is a package that enhances Node.js
+const cors = require('cors'); //Cross-Origin Resource Sharing defines origins for sharing for security reasons
+const superagent = require('superagent'); //superagent handles API requests
+const pg = require('pg'); //postgres allows for database creation
+const PORT = process.env.PORT || 3000; //Sets the port indicated in the .env file and a fallback port if that's not found
+const app = express(); //Sets "app" to the express function
+app.use(cors()); //Tells Express to use the cors package
 
 //Configure Database
-const client = new pg.Client(process.env.DATABASE_URL);
-client.on('err', err => console.error(err));
+const client = new pg.Client(process.env.DATABASE_URL); //Sets "client" to the city_explorer database object (city_explorer is the database references in the URL in the .env)
+client.on('err', err => console.error(err)); //If there is an error connecting to the data, this function logs that error
 
-//Errors
+//Error Handlers
 function notFoundHandler(request,response) {
   response.status(404).send('huh?');
 }
@@ -23,62 +24,62 @@ function errorHandler(error,request,response) {
   response.status(500).send(error);
 }
 
-//Constructor Functions
+//Location Constructor
 function Location(query, data){
-  this.search_query = query;
-  this.formatted_query = data.formatted_address;
-  this.latitude = data.geometry.location.lat;
-  this.longitude = data.geometry.location.lng;
+  this.search_query = query; //Returns "search_query: 'madras, or'," when "Madras, OR" searched for on site (this Madras instantiation happens in the Location.fetchLocation function below)
+  this.formatted_query = data.formatted_address; //Returns "'Madras, OR 97741, USA'"
+  this.latitude = data.geometry.location.lat; //Returns "44.6334544" for Madras
+  this.longitude = data.geometry.location.lng; //Returns "-121.1294872" for Madras
 }
 
-//Define a prototype function to save data to DB
+//Location prototype constructor function that saves instantiated location to the database (called in Location.fetchLocation below if not aleady in database)
 Location.prototype.save = function(){
   const SQL = `INSERT INTO locations
   (search_query, formatted_query, latitude, longitude)
   VALUES ($1, $2, $3, $4)
-  RETURNING *`;
-
+  RETURNING *`; //Adds searched for location as a new row in the locations table
   let values = Object.values(this);
   return client.query(SQL, values);
 };
 
-//My Static Constructor Functions
-
+//Location static constructor function that gets information from the API
 Location.fetchLocation = function (query){
   const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${query}&key=${process.env.GEOCODE_API_KEY}`;
-
   return superagent.get(url)
-    .then( result=> {
+    .then( result => {
+      //if what's returned from the API is empty, throw a 'No data' message (e.g. if you enter "jkjkjk" in location you will get this message in the terminal)
       if(!result.body.results.length) {throw 'No data';}
-      let location = new Location(query, result.body.results[0]);
-      return location.save()
+      let location = new Location(query, result.body.results[0]); //"body" is indicating entirety of what superagent gets from the API; "results" is the name of the first key in the geocoding data itself as seen in the sample here (https://developers.google.com/maps/documentation/geocoding/start); [0] is because you're only searching for one location at a time, so there's nothing after index zero
+      return location.save()//Uses the function above to add a row to the database for the location
         .then( result => {
-          location.id = result.rows[0].id; //update, delete...etc...
+          location.id = result.rows[0].id; //Sets the location ID column for the new location row in the table (the number is auto-incremented via "serial" in the schema)
           return location;
         });
     });
 };
 
+//Location static constructor function that checks to see if searched for location is already in the table
 Location.lookup = (handler) => {
-  const SQL = `SELECT * FROM locations WHERE search_query=$1`;
-  const values = [handler.query];
-
+  const SQL = `SELECT * FROM locations WHERE search_query=$1`; //$1 is what was searched for on the site (e.g. 'madras, or')
+  const values = [handler.query]; //Returns "[ 'madras, or' ]" - QUESTION: Why does this need to be an array? The postgres documentation about client.query isn't clear to me
   return client.query(SQL, values)
     .then( results => {
-      if (results.rowCount > 0){
-        handler.cacheHit(results);
-      }else {
-        handler.cacheMiss();
+      if (results.rowCount > 0){ //If the location is already in the database, the results.rowCount for the location will equal 1; if not in the database it will equal 0
+        handler.cacheHit(results); //Uses cacheHit function to log "Got data from DB" to the console
+      } else {
+        handler.cacheMiss(); ////Uses cacheMiss function to log "No data in DB, fetching..." to the console
       }
     })
     .catch(console.error);
 };
 
+//Weather Constructor
 function Weather(day) {
   this.forecast = day.summary;
   this.time = new Date(day.time * 1000).toString().slice(0,15);
 }
 
+//Movies Constructor
 function Movies(movie) {
   this.title = movie.title;
   this.overview = movie.overview;
@@ -90,6 +91,7 @@ function Movies(movie) {
   this.created_on = Date.now();
 }
 
+//Yelp Constructor
 function Yelp(review) {
   this.name = review.name;
   this.rating = review.rating;
@@ -99,25 +101,20 @@ function Yelp(review) {
   this.created_at = Date.now();
 }
 
-// API Routes
-
+//API Routes
 app.get('/location', getLocation);
 app.get('/weather', getWeather);
 app.get('/movies', getMovies);
 app.get('/yelp', getYelp);
 
 //Route Handlers
-
 function getLocation(request,response) {
-
   const locationHandler = {
     query: request.query.data,
-
     cacheHit: (results) => {
       console.log('Got data from DB');
       response.send(results.rows[0]);
     },
-
     cacheMiss: () => {
       console.log('No data in DB, fetching...');
       Location.fetchLocation(request.query.data)
@@ -148,6 +145,7 @@ function getMovies(request, response) {
       const movieSummaries = data.body.results.map(movie => {
         return new Movies(movie);
       });
+      console.log(movieSummaries);
       response.status(200).json(movieSummaries);
     })
     .catch( ()=> {
@@ -156,17 +154,14 @@ function getMovies(request, response) {
 }
 
 function getYelp(request, response) {
-
-  const url = `https://api.yelp.com/v3/businesses/search?latitude=${request.query.data.latitude}&longitude=${request.query.data.longitude}`;
-
-  console.log(request);
+  const url = `https://api.yelp.com/v3/businesses/search?location=${request.query.data.search_query}`;
   console.log(url);
   superagent.get(url)
     .set('Authorization', `Bearer ${process.env.YELP_API_KEY}`)
     .then(result => {
       const yelpSummaries = result.body.businesses.map(review => {
         const summary = new Yelp(review);
-        // summary.save(location.id);
+        // console.log(summary);
         return summary;
       });
       return yelpSummaries;
@@ -179,12 +174,12 @@ function getYelp(request, response) {
 app.use('*', notFoundHandler);
 app.use(errorHandler);
 
-// HELPER FUNCTIONS
-
 // Make sure the server is listening for requests
 client.connect()
   .then( ()=> {
     app.listen(PORT, ()=> {
-      console.log('server and db are up, listening on port ', PORT);
+      console.log('server and db are up, listening on port', PORT);
     });
   });
+
+
