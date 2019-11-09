9422c4211acb8d298d561e61d5a274bde0599f7d
Holly Davis <hollydavis541@gmail.com>
2019-11-09 10:53:17 -0800

    created Trail constructor and working getTrails function

--- server.js
+++ server.js
@@ -99,12 +99,27 @@ function Yelp(review) {
   this.created_at = Date.now();
 }
 
+function Trail(trail) {
+  this.name = trail.name;
+  this.location = trail.location;
+  this.length = trail.length;
+  this.stars = trail.stars;
+  this.star_votes = trail.starVotes;
+  this.summary = trail.summary;
+  this.trail_url = trail.url;
+  this.conditions = trail.conditionStatus;
+  this.condition_date = trail.conditionDate;
+  this.condition_time = trail.conditionDate;
+  this.created_at = Date.now();
+}
+
 // API Routes
 
 app.get('/location', getLocation);
 app.get('/weather', getWeather);
 app.get('/movies', getMovies);
 app.get('/yelp', getYelp);
+app.get('/trails', getTrails);
 
 //Route Handlers
 
@@ -173,6 +188,23 @@ function getYelp(request, response) {
     })
     .catch( ()=> {
       errorHandler('No movies for you!', request, response);
+    });
+}
+
+function getTrails(request, response) {
+  const url = `https://www.hikingproject.com/data/get-trails?lat=${request.query.data.latitude}&lon=${request.query.data.longitude}&key=${process.env.TRAIL_API_KEY}`
+  console.log(url);
+  superagent.get(url)
+    .then( data => {
+      const trailSummaries = data.body.trails.map(trail => {
+        const summary = new Trail(trail);
+        console.log(summary);
+        return summary
+      });
+      response.status(200).json(trailSummaries);
+    })
+    .catch( ()=> {
+      errorHandler('No trails for you!', request, response);
     });
 }
 
