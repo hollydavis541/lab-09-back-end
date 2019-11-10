8a7421be75f0d0221d7b16cb04c61d314dd244dd
Holly Davis <hollydavis541@gmail.com>
2019-11-09 16:20:26 -0800

    modded locations

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const pg = require('pg');
 const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(cors());
+const Location = require('./locations');
 
 //Configure Database
 const client = new pg.Client(process.env.DATABASE_URL);
@@ -24,55 +25,57 @@ function errorHandler(error,request,response) {
 }
 
 //Constructor Functions
-function Location(query, data){
-  this.search_query = query;
-  this.formatted_query = data.formatted_address;
-  this.latitude = data.geometry.location.lat;
-  this.longitude = data.geometry.location.lng;
-}
+// function Location(query, data){
+//   this.search_query = query;
+//   this.formatted_query = data.formatted_address;
+//   this.latitude = data.geometry.location.lat;
+//   this.longitude = data.geometry.location.lng;
+// }
 
-//Define a prototype function to save data to DB
-Location.prototype.save = function(){
-  const SQL = `INSERT INTO locations
-  (search_query, formatted_query, latitude, longitude)
-  VALUES ($1, $2, $3, $4)
-  RETURNING *`;
+// //Define a prototype function to save data to DB
+// Location.prototype.save = function(){
+//   const SQL = `INSERT INTO locations
+//   (search_query, formatted_query, latitude, longitude)
+//   VALUES ($1, $2, $3, $4)
+//   RETURNING *`;
 
-  let values = Object.values(this);
-  return client.query(SQL, values);
-};
+//   let values = Object.values(this);
+//   return client.query(SQL, values);
+// };
 
-//My Static Constructor Functions
+// //My Static Constructor Functions
 
-Location.fetchLocation = function (query){
-  const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${query}&key=${process.env.GEOCODE_API_KEY}`;
+// Location.fetchLocation = function (query){
+//   const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${query}&key=${process.env.GEOCODE_API_KEY}`;
 
-  return superagent.get(url)
-    .then( result=> {
-      if(!result.body.results.length) {throw 'No data';}
-      let location = new Location(query, result.body.results[0]);
-      return location.save()
-        .then( result => {
-          location.id = result.rows[0].id; //update, delete...etc...
-          return location;
-        });
-    });
-};
+//   return superagent.get(url)
+//     .then( result=> {
+//       if(!result.body.results.length) {throw 'No data';}
+//       let location = new Location(query, result.body.results[0]);
+//       return location.save()
+//         .then( result => {
+//           location.id = result.rows[0].id; //update, delete...etc...
+//           return location;
+//         });
+//     });
+// };
 
-Location.lookup = (handler) => {
-  const SQL = `SELECT * FROM locations WHERE search_query=$1`;
-  const values = [handler.query];
+// Location.lookup();
 
-  return client.query(SQL, values)
-    .then( results => {
-      if (results.rowCount > 0){
-        handler.cacheHit(results);
-      }else {
-        handler.cacheMiss();
-      }
-    })
-    .catch(console.error);
-};
+// Location.lookup = (handler) => {
+//   const SQL = `SELECT * FROM locations WHERE search_query=$1`;
+//   const values = [handler.query];
+
+//   return client.query(SQL, values)
+//     .then( results => {
+//       if (results.rowCount > 0){
+//         handler.cacheHit(results);
+//       }else {
+//         handler.cacheMiss();
+//       }
+//     })
+//     .catch(console.error);
+// };
 
 function Weather(day) {
   this.forecast = day.summary;
